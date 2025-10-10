6d678919769f0760c3200ef709fd4afdcc14ed4c
Edoardo Marangoni <ecmm@anche.no>
2025-10-10 14:41:47 +0200

    rustc_mir_transform: replace usages of `pointer_width` with `pointer_offset()`

--- compiler/rustc_mir_transform/src/unreachable_prop.rs
+++ compiler/rustc_mir_transform/src/unreachable_prop.rs
@@ -94,8 +94,12 @@ pub(crate) fn remove_successors_from_switch<'tcx>(
 
     let discr_ty = discr.ty(body, tcx);
     let discr_size = Size::from_bits(match discr_ty.kind() {
-        ty::Uint(uint) => uint.normalize(tcx.sess.target.pointer_width).bit_width().unwrap(),
-        ty::Int(int) => int.normalize(tcx.sess.target.pointer_width).bit_width().unwrap(),
+        ty::Uint(uint) => {
+            uint.normalize(tcx.data_layout.pointer_offset().bits() as _).bit_width().unwrap()
+        }
+        ty::Int(int) => {
+            int.normalize(tcx.data_layout.pointer_offset().bits() as _).bit_width().unwrap()
+        }
         ty::Char => 32,
         ty::Bool => 1,
         other => bug!("unhandled type: {:?}", other),
