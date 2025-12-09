2cefcb1c4fbaa6b3736f9b7d047a254c137e65f4
Edoardo Marangoni <ecmm@anche.no>
2025-12-09 10:52:21 +0100

    cheri: set default channel in bootstrap.toml (#49)
    + small fixes to selected backend when running in CI on main.

--- cheri/gen_bootstrap.sh
+++ cheri/gen_bootstrap.sh
@@ -17,6 +17,35 @@ elif [ -z "${CUSTOM_CMAKE_FLAGS+set}" ]; then
         CUSTOM_CMAKE_FLAGS=""
 fi
 
+# Check if we're running CI for a PR to main.
+if [ -n "${CIRRUS_BASE_BRANCH+set}" ]; then
+    if [ "$CIRRUS_BASE_BRANCH" == "main" ]; then
+        CHANNEL="nightly"
+        # We are seeing some faults unrelated to our changes when CI runs from
+        # main, specifically tied to gcc. When running like that (CI from main)
+        # we want to restrict the backend to LLVM and cranelift only, so that
+        # we don't get stuck on things that aren't our fault. Ideally, we
+        # should restore the normal behaviour once the error is fixed.
+        CODEGEN_BACKENDS="codegen-backends = [\"llvm\",\"cranelift\"]"
+    else
+        # TODO: change this to `dev` once we add rustfmt to the beta branch, so
+        # that fmt --check is part of the tests.
+        CHANNEL="beta"
+    fi
+# Otherwise we may be running for a direct push to main.
+elif [ -n "${CIRRUS_BRANCH+set}" ]; then
+    if [ "$CIRRUS_BRANCH" == "main" ]; then
+        CHANNEL="nightly"
+
+        # See comment above.
+        CODEGEN_BACKENDS="codegen-backends = [\"llvm\",\"cranelift\"]"
+    else
+        CHANNEL="beta"
+    fi
+else
+        CHANNEL="beta"
+fi
+
 if [ -e "$FILE" ]; then
   echo "$FILE already exists!"
   exit 1
@@ -26,14 +55,14 @@ cat > "$FILE" <<- EOF
 # See bootstrap.example.toml for documentation of available options
 #
 profile = "compiler"  # Includes one of the default files in src/bootstrap/defaults
-change-id = 140732
+change-id = "ignore"
 
 [build]
 ccache = true
 
 [rust]
-channel = "beta"
-#codegen-backends = ["llvm"]
+channel = "$CHANNEL"
+$CODEGEN_BACKENDS
 #debug = true
 #debuginfo-level = 2
 std-features = ["compiler-builtins-mem"]
