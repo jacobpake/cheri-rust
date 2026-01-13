c014c2ae070e70be5993a1676ee8ef85805ab961
Jacob Pake <github@pake.dev>
2026-01-13 09:25:12 +0000

    separate stderr shim

--- library/std/src/sys/stdio/cheriotrtos.rs
+++ library/std/src/sys/stdio/cheriotrtos.rs
@@ -6,16 +6,23 @@ use crate::ffi::CString;
 use crate::io::{self};
 
 unsafe extern "C" {
-    fn cheriot_print_str(v: *const core::ffi::c_char);
+    fn cheriot_print_str_stdout(v: *const core::ffi::c_char);
+    fn cheriot_print_str_stderr(v: *const core::ffi::c_char);
 }
 
 pub type Stdin = unsupported_stdio::Stdin;
 pub struct Stdout;
-pub type Stderr = Stdout;
+pub struct Stderr;
 
 impl Stdout {
     pub const fn new() -> Stdout {
         Stdout
+    }
+}
+
+impl Stderr {
+    pub const fn new() -> Stderr {
+        Stderr
     }
 }
 
@@ -24,7 +31,23 @@ impl io::Write for Stdout {
         let cstr = CString::new(buf).unwrap();
 
         unsafe {
-            cheriot_print_str(cstr.as_ptr());
+            cheriot_print_str_stdout(cstr.as_ptr());
+        }
+
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+impl io::Write for Stderr {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let cstr = CString::new(buf).unwrap();
+
+        unsafe {
+            cheriot_print_str_stderr(cstr.as_ptr());
         }
 
         Ok(buf.len())
