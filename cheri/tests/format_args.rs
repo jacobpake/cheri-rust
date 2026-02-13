e0de5d94ff3410ecdfe48e0a1d86f3ae4bc9a3d2
Edoardo Marangoni <ecmm@anche.no>
2026-02-13 09:41:03 +0100

    cheri/tests: add test of alloc::format! with 'alternate' flag

--- cheri/tests/format_args.rs
+++ cheri/tests/format_args.rs
@@ -1,5 +1,6 @@
 #![no_std]
 
+extern crate alloc;
 extern crate cheriot;
 
 #[no_mangle]
@@ -7,5 +8,7 @@ extern "C" fn test_format_args() -> i32 {
     assert_eq!(format_args!("hello").as_str(), Some("hello"));
     assert_eq!(format_args!("").as_str(), Some(""));
     assert_eq!(format_args!("{:?}", cheriot::rand::rand()).as_str(), None);
+    let x = 42;
+    assert_eq!(alloc::format!("{x:#b}"), "0b101010");
     0
 }
