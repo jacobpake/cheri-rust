cc2e22df5c751c7103972dff7ff600895e55a7d1
Edoardo Marangoni <ecmm@anche.no>
2026-05-07 10:28:34 +0200

    coretests: adapt tests checking size/alignment of *const T to CHERIoT

--- library/coretests/tests/mem.rs
+++ library/coretests/tests/mem.rs
@@ -25,9 +25,14 @@ fn size_of_16() {
 
 #[test]
 #[cfg(target_pointer_width = "32")]
-#[cfg_attr(target_abi = "cheriot", ignore)] // FIXME(cheri/triage): pointer width
 fn size_of_32() {
     assert_eq!(size_of::<usize>(), 4);
+    // On CHERIoT and other CHERI platforms, the size and alignment
+    // of `usize` are not the same as those of `*const T`.
+    #[cfg(target_family = "cheriot")]
+    assert_eq!(size_of::<*const usize>(), 8);
+
+    #[cfg(not(target_family = "cheriot"))]
     assert_eq!(size_of::<*const usize>(), 4);
 }
 
@@ -62,9 +67,15 @@ fn align_of_16() {
 
 #[test]
 #[cfg(target_pointer_width = "32")]
-#[cfg_attr(target_abi = "cheriot", ignore)] // FIXME(cheri/triage): pointer width
 fn align_of_32() {
     assert_eq!(align_of::<usize>(), 4);
+
+    // On CHERIoT and other CHERI platforms, the size and alignment
+    // of `usize` are not the same as those of `*const T`.
+    #[cfg(target_family = "cheriot")]
+    assert_eq!(align_of::<*const usize>(), 8);
+
+    #[cfg(not(target_family = "cheriot"))]
     assert_eq!(align_of::<*const usize>(), 4);
 }
 
